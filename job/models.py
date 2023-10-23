9b7befd76318fdd965416420c33900f0f81a7323
Nooraldin2001 <noorabdalkadir@std.mans.edu.eg>
2023-10-23 22:05:39 +0300

    adding slug to models

--- job/models.py
+++ job/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django_countries.fields import CountryField
 from django.utils import timezone
-
+from django.utils.text import slugify
 
 
 job_TYPE = [
@@ -26,9 +26,16 @@ class Job(models.Model):
     job_type = models.CharField(choices=job_TYPE, max_length=10)
     experience = models.IntegerField()
     category = models.ForeignKey('Category', related_name='job_category', on_delete= models.SET_NULL, null=True, blank=True)
+    slug = models.SlugField(null=True,blank=True)
 
     def __str__(self):
         return self.title
+    
+
+
+    def save(self, *args, **kwargs):
+       self.slug = slugify(self.title)
+       super(Job, self).save(*args, **kwargs) # Call the real save() method
 
 
 
