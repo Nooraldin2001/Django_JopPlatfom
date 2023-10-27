034a7c25c8b7ffacd1ce91e4c4a32132599ff67e
Nooraldin2001 <noorabdalkadir@std.mans.edu.eg>
2023-10-27 09:28:51 +0200

    adding job_detail

--- job/urls.py
+++ job/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import job_list
+from .views import job_list, job_detail
 
 urlpatterns = [
     path('', job_list),
+    path('<slug:slug>', job_detail),
 ]
