80ba51a1302db8516850d2e77043d06806f3e3e7
Nooraldin2001 <noorabdalkadir@std.mans.edu.eg>
2023-11-02 13:44:52 +0200

    adding paggination

--- job/views.py
+++ job/views.py
@@ -1,10 +1,25 @@
 from django.shortcuts import render
 from .models import Job
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 # Create your views here.
 
 def job_list(request):
+
     all_jobs = Job.objects.all()
-    return render(request,'job/job_list.html',{'jobs':all_jobs})
+
+    jobs_count = all_jobs.count()
+
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(all_jobs, 50)
+    try:
+        all_jobs = paginator.page(page)
+    except PageNotAnInteger:
+        all_jobs = paginator.page(1)
+    except EmptyPage:
+        all_jobs = paginator.page(paginator.num_pages)
+    return render(request,'job/job_list.html',{'jobs':all_jobs, 'jobs_count':jobs_count})
+
 
 
 def job_detail(request, slug):
